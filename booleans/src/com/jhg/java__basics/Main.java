6515bbafc856191150358659d7631b35b5163f19
Jesse <jessehglvn@gmail.com>
2023-08-16 21:20:20 -0400

    booleans

---
+++ booleans/src/com/jhg/java__basics/Main.java
@@ -0,0 +1,22 @@
+package com.jhg.java__basics;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean isRaining = true;
+        boolean isVegetable = false;
+
+      //Boolean Expressions
+        int x = 4;
+        int y = 9;
+
+        System.out.println(isRaining);
+        System.out.println(isVegetable);
+
+        System.out.println(x>y);
+        System.out.println(y==x);
+        System.out.println(y>x);
+        
+    }
+}
--- data-types/src/com/jhg/java__basics/Main.java
+++ data-types/src/com/jhg/java__basics/Main.java
@@ -7,20 +7,20 @@ public class Main {
     public static void main(String[] args) {
 
         //numeric data types
-            int num1 = 32;
-            short num2 = 9;
-            float num3 = 45.006f;
-            double num4 = 43.79;
-            long num5 = 5;
+        int num1 = 59;
+        short num2 = 5;
+        float num3 = 49.007f;
+        double num4 = 49.04;
+        long num5 = 9;
 
-        //Round decimal to 2 places
-            DecimalFormat df = new DecimalFormat();
-            df.setMaximumFractionDigits(2);
+        //2 decimal points
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
 
-	    System.out.println("Value of num1: " + num1 + " - Size of int is " + (Integer.SIZE/8) + " bytes");
-        System.out.println("Value of num2: " + num2 + " - Size of short is " + (Short.SIZE/8) + " bytes");
-        System.out.println("Value of num3: " + (df.format(num3)) + " - Size of float is " + (Float.SIZE/8) + " bytes");
-        System.out.println("Value of num4: " + num4 + " - Size of double is " + (Double.SIZE/8) + " bytes");
-        System.out.println("Value of num5: " + num5 + " - Size of long is " + (Long.SIZE/8) + " bytes");
+        System.out.println("Value of num1: " + num1 + " - Size of int: " + (Integer.SIZE/8) + " Bytes");
+        System.out.println("Value of num2: " + num2 + " - Size of Short " + (Short.SIZE/8) + " Bytes");
+        System.out.println("Value of num3: " + df.format(num3) + " - Size of Float " + (Float.SIZE/8) + " Bytes");
+        System.out.println("Value of num4: " + num4 + " - Size of Double " + (Double.SIZE/8) + " Bytes");
+        System.out.println("Value of num5: " + num5 + " - Size of Long " + (Long.SIZE/8) + " Bytes");
     }
 }
