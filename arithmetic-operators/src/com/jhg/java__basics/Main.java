69366d04d32d56eeebf887b4323bc2afb789f5a9
Jesse <jessehglvn@gmail.com>
2023-08-20 22:57:38 -0400

    quotient decimal format

--- arithmetic-operators/src/com/jhg/java__basics/Main.java
+++ arithmetic-operators/src/com/jhg/java__basics/Main.java
@@ -1,23 +1,27 @@
 package com.jhg.java__basics;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	int a;
-	int b;
-	int result;
+	float a;
+	float b;
+	float result;
+
+        DecimalFormat df = new DecimalFormat();
+
 	Scanner scanner = new Scanner(System.in);
 	System.out.println("Enter a number");
 	a = scanner.nextInt();
 	System.out.println("Enter a number");
 	b = scanner.nextInt();
 
-	result = a+b;
+	result = a/b;
 
 	System.out.println("the sum is " + result);
-	System.out.printf("the sum is %d", result);
+	System.out.printf("the sum is %.2f", result);
 
     }
 }
